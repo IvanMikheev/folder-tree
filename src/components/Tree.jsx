edb123352c3152216736bccb5849540247c9e04c
IvanMikheev <miv-94@yandex.ru>
2019-10-05 21:02:42 +0300

    Basic functionality

--- src/components/Tree.jsx
+++ src/components/Tree.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import TreeNode from './TreeNode'
 
-const URL = 'https://raw.githubusercontent.com/wrike/frontend-test/master/data.json'; 
+const URL = 'https://raw.githubusercontent.com/wrike/frontend-test/master/data.json';
+const URL2 = 'https://next.json-generator.com/api/json/get/NkQZN2Z_D';
 
 class Tree extends Component {
   constructor(props) {
@@ -10,39 +11,64 @@ class Tree extends Component {
     this.state = {
       nodes: [],
       isLoading: false,
-      errors: null
+      error: null,
+      nestedTree: null
     };
   }
 
   componentDidMount() {
     this.setState({ isLoading: true });
 
-    fetch(URL)
-      .then(response => response.json())
+    fetch(URL2)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Whoops... Error occurred');
+        }
+      })
       .then(nodes => this.setState({ nodes, isLoading: false }))
       .catch(e => this.setState({ isLoading: false, error: e }));
   }
 
   render() {
-    const { nodes, isLoading } = this.state;    
+    const { nodes, isLoading, error } = this.state;
 
-    if (isLoading) {      
-      return <p>Loading ...</p>;      
-    } 
-    
-    return (
-      <div style = { Style }>        
-        {nodes.map( (node,i) => 
-          <TreeNode id={node.id} title={node.title} level={i}/>
-        )}
-      </div>
-    )
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    if (nodes.filter(node => node.id === -1).length !== 1) {
+      return <p>Wrong Tree!</p>;
+
+    } else {
+      let root = nodes.filter(node => node.id === -1)[0];
+      root.childs = AddChilds(root, nodes);
+
+      return (
+        <div style={Style}>
+          <TreeNode node={root} level={0} />
+        </div>
+      )
+    }
   }
 }
 
 const Style = {
   width: "1000px",
   margin: "0 auto"
+}
+
+const AddChilds = (node, templateNodes) => {  
+  const childs = templateNodes.filter(child => node.id === child.parentId);
+  childs.forEach(item => {
+    item.childs = AddChilds(item, templateNodes);
+  });  
+  return childs;
 }
 
 export default Tree
--- src/components/TreeNode.jsx
+++ src/components/TreeNode.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
 
 class TreeNode extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: null,
+      title: '',
+      parentId: null,
+      level: null,
+      childs: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ 
+      id: this.props.node.id,
+      title: this.props.node.title,
+      parentId: this.props.node.parentId,
+      level: this.props.level,
+      childs: this.props.node.childs
+     });
+  }
+
   render() {
     return (
-      <div>
-        {this.props.title}
+      <div style={{ paddingLeft: 20 }}>
+        {this.state.title}
+        {this.state.childs.map( child => 
+          <TreeNode node={child} level={this.state.level + 1} />
+        )}
       </div>
     )
   }
 }
+
+
 
 export default TreeNode
